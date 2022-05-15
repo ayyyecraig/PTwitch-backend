d2bc1299a3f6b501804271443aba812c4e000e4a
Adelaide <adelaide.craig@outlook.com>
2022-05-15 14:53:57 -0500

    comment please

--- seeders/20220508030216-comments.js
+++ seeders/20220508030216-comments.js
@@ -2,6 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'comments',
+      [
+        {
+         userId: 1,
+         streamerId: 2,
+         content: 'Based AF',
+         createdAt: new Date(),
+         updatedAt: new Date() 
+        },
+        {
+          userId: 1,
+          streamerId: 1,
+          content: 'SHEEEESSHHHH',
+          createdAt: new Date(),
+          updatedAt: new Date() 
+         }
+        ]
+    )
   },
 
  down: async (queryInterface, Sequelize) => {
